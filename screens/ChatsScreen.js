6cb424ccfc4fd17dc83f0546be1f18059d4f2db5
Cendekia Pramana Putra <me@cendekiapp.com>
2017-09-27 18:43:57 +0700

    add default content on chat screen

--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import Header from '../components/headers/DefaultHeader';
+import { Container, Content, Text } from "native-base";
 
 export default class ChatsScreen extends React.Component {
   static navigationOptions = {
@@ -10,19 +11,13 @@ export default class ChatsScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <Container>
+        <Content style={{justifyContent: 'center', alignItems: 'center' }}>
+          <Text>
+            Chat Bot screen
+          </Text>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
